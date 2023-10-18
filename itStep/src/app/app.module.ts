8f844fcf03762a26f8ce13823680830a26003872
sabakavtaradze <sabakavtaradzee@gmail.com>
2023-10-18 17:59:18 +0400

    updates

--- itStep/src/app/app.module.ts
+++ itStep/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @NgModule({
   declarations: [
@@ -35,6 +36,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    MatButtonModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
